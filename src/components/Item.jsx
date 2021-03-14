8911a907dc38848b9e24beea3c481eebcfd5487c
vitkuzmenko <kuzmenko.v.u@gmail.com>
2021-03-14 19:45:58 +0300

    remove RootRef for mui v5

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MenuItem from '@material-ui/core/MenuItem';
-import RootRef from '@material-ui/core/RootRef';
 
 class Item extends React.PureComponent {
   render() {
     const {
-      name, iso2, dialCode, localization, itemRef, native, ...restProps
+      name,
+      iso2,
+      dialCode,
+      localization,
+      itemRef,
+      native,
+      ...restProps
     } = this.props;
 
     if (native) {
@@ -26,22 +31,21 @@ class Item extends React.PureComponent {
     }
 
     return (
-      <RootRef rootRef={(node) => itemRef(node)}>
-        <MenuItem
-          className="country"
-          data-dial-code="1"
-          data-country-code={iso2}
-          {...restProps}
-        >
-          <div className={`flag ${iso2} margin`} />
+      <MenuItem
+        ref={(node) => itemRef(node)}
+        className="country"
+        data-dial-code="1"
+        data-country-code={iso2}
+        {...restProps}
+      >
+        <div className={`flag ${iso2} margin`}/>
 
-          <span className="country-name">
-            {localization || name}
-          </span>
+        <span className="country-name">
+          {localization || name}
+        </span>
 
-          <span className="dial-code">{`+${dialCode}`}</span>
-        </MenuItem>
-      </RootRef>
+        <span className="dial-code">{`+${dialCode}`}</span>
+      </MenuItem>
     );
   }
 }
